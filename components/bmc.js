f741814f3fad0d75bd8712bb52580013303d9de9
cantinacsm <buebue55@outlook.com>
2026-04-01 13:35:02 -0300

    Update bmc.js

--- components/bmc.js
+++ components/bmc.js
@@ -1,34 +1,29 @@
-(function(){
+(function () {
 
-  // evita carregar duas vezes
-  if(window.bmcLoaded) return;
+  if (window.bmcLoaded) return;
   window.bmcLoaded = true;
 
-  // não carregar no mobile (remova se quiser mostrar)
-  if(window.innerWidth <= 700) return;
-
-  function loadBMC(){
+  function loadWidget() {
 
     const script = document.createElement("script");
 
     script.src =
       "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js";
 
-    script.setAttribute("data-name","BMC-Widget");
-    script.setAttribute("data-cfasync","false");
+    script.setAttribute("data-name", "BMC-Widget");
+    script.setAttribute("data-cfasync", "false");
 
-    script.setAttribute("data-id","ViquinhoDev");
-    script.setAttribute("data-description",
-      "Support me on Buy me a coffee!");
-    script.setAttribute("data-message","support me");
-    script.setAttribute("data-color","#BD5FFF");
-    script.setAttribute("data-position","Right");
-    script.setAttribute("data-x_margin","18");
-    script.setAttribute("data-y_margin","18");
+    script.setAttribute("data-id", "ViquinhoDev");
+    script.setAttribute("data-description", "Support me on Buy me a coffee!");
+    script.setAttribute("data-message", "support me");
+    script.setAttribute("data-color", "#BD5FFF");
+    script.setAttribute("data-position", "Right");
+    script.setAttribute("data-x_margin", "18");
+    script.setAttribute("data-y_margin", "18");
 
     document.body.appendChild(script);
   }
 
-  window.addEventListener("load", loadBMC);
+  window.addEventListener("load", loadWidget);
 
 })();
