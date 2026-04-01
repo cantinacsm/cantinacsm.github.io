7a1138bde8e33a0d0259ec53c66701d281f8d6b5
cantinacsm <buebue55@outlook.com>
2026-04-01 13:28:22 -0300

    Create bmc.js

---
+++ components/bmc.js
@@ -0,0 +1,34 @@
+(function(){
+
+  // evita carregar duas vezes
+  if(window.bmcLoaded) return;
+  window.bmcLoaded = true;
+
+  // não carregar no mobile (remova se quiser mostrar)
+  if(window.innerWidth <= 700) return;
+
+  function loadBMC(){
+
+    const script = document.createElement("script");
+
+    script.src =
+      "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js";
+
+    script.setAttribute("data-name","BMC-Widget");
+    script.setAttribute("data-cfasync","false");
+
+    script.setAttribute("data-id","ViquinhoDev");
+    script.setAttribute("data-description",
+      "Support me on Buy me a coffee!");
+    script.setAttribute("data-message","support me");
+    script.setAttribute("data-color","#BD5FFF");
+    script.setAttribute("data-position","Right");
+    script.setAttribute("data-x_margin","18");
+    script.setAttribute("data-y_margin","18");
+
+    document.body.appendChild(script);
+  }
+
+  window.addEventListener("load", loadBMC);
+
+})();
